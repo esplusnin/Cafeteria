5cea55173d4214bb05df6bae4ce377a7fe5f8253
esplusnin <esplusnin94@yandex.ru>
2024-02-08 11:58:20 +0400

    #3 Добавить строки, пикчи на все экраны

--- Cafeteria/Generated/Strings+Generated.swift
+++ Cafeteria/Generated/Strings+Generated.swift
@@ -10,6 +10,61 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
+  internal enum Enter {
+    /// e-mail
+    internal static let email = L10n.tr("Localizable", "enter.email", fallback: "e-mail")
+    /// Войти
+    internal static let enter = L10n.tr("Localizable", "enter.enter", fallback: "Войти")
+    /// Пароль
+    internal static let password = L10n.tr("Localizable", "enter.password", fallback: "Пароль")
+    /// Вход
+    internal static let title = L10n.tr("Localizable", "enter.title", fallback: "Вход")
+  }
+  internal enum Map {
+    /// Карта
+    internal static let title = L10n.tr("Localizable", "map.title", fallback: "Карта")
+  }
+  internal enum Menu {
+    /// Капучино
+    internal static let cappuccino = L10n.tr("Localizable", "menu.cappuccino", fallback: "Капучино")
+    /// Эспрессо
+    internal static let espresso = L10n.tr("Localizable", "menu.espresso", fallback: "Эспрессо")
+    /// Перейти к оплате
+    internal static let goToPay = L10n.tr("Localizable", "menu.goToPay", fallback: "Перейти к оплате")
+    /// Горячий шоколад
+    internal static let hotChocolate = L10n.tr("Localizable", "menu.hotChocolate", fallback: "Горячий шоколад")
+    /// Латте
+    internal static let latte = L10n.tr("Localizable", "menu.latte", fallback: "Латте")
+    /// Меню
+    internal static let title = L10n.tr("Localizable", "menu.title", fallback: "Меню")
+  }
+  internal enum NearestCafeterian {
+    /// от вас
+    internal static let fromYou = L10n.tr("Localizable", "nearestCafeterian.fromYou", fallback: "от вас")
+    /// На карте
+    internal static let onMap = L10n.tr("Localizable", "nearestCafeterian.onMap", fallback: "На карте")
+    /// Ближайшие кофейня
+    internal static let title = L10n.tr("Localizable", "nearestCafeterian.title", fallback: "Ближайшие кофейня")
+  }
+  internal enum Order {
+    /// 
+    internal static let pay = L10n.tr("Localizable", "order.pay", fallback: "")
+    /// Время ожидания заказа 15 минут
+    /// Спасибо, что выбрали нас!
+    internal static let thanks = L10n.tr("Localizable", "order.thanks", fallback: "Время ожидания заказа 15 минут\nСпасибо, что выбрали нас!")
+    /// Ваш заказ
+    internal static let title = L10n.tr("Localizable", "order.title", fallback: "Ваш заказ")
+  }
+  internal enum Registration {
+    /// e-mail
+    internal static let email = L10n.tr("Localizable", "registration.email", fallback: "e-mail")
+    /// Пароль
+    internal static let password = L10n.tr("Localizable", "registration.password", fallback: "Пароль")
+    /// Регистрация
+    internal static let registration = L10n.tr("Localizable", "registration.registration", fallback: "Регистрация")
+    /// Повторите пароль
+    internal static let repeatPassword = L10n.tr("Localizable", "registration.repeatPassword", fallback: "Повторите пароль")
+  }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
