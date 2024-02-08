ebf27959302a5cf7a2b921a9c153c7d403d01c99
esplusnin <esplusnin94@yandex.ru>
2024-02-08 18:40:08 +0400

    #10 Сверстать экрана входа

--- Cafeteria/Modules/Enter/EnterViewController.swift
+++ Cafeteria/Modules/Enter/EnterViewController.swift
@@ -1,14 +1,14 @@
 import UIKit
 
-class RegistationViewController: UIViewController {
-
+final class EnterViewController: UIViewController {
+    
     // MARK: - Constants and Variables:
     private enum LocalUIConstants {
         static let inputsStacksInset: CGFloat = 24
         static let buttonTopInset: CGFloat = 30
     }
     
-    // MARK: - UI:
+    // MARK: UI:
     private lazy var inputsStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
@@ -26,59 +26,45 @@ class RegistationViewController: UIViewController {
         return stackView
     }()
     
-    private lazy var repeatPasswordStackView: CustomInputStackView = {
-        let stackView = CustomInputStackView(delegate: self, state: .repeatPassword)
-        return stackView
-    }()
-    
-    private lazy var registrationButton: CustomButton = {
+    private lazy var enterButton: CustomButton = {
         let button = CustomButton(type: .system)
         button.setTitle(L10n.Registration.registration, for: .normal)
         return button
     }()
-    
-    // MARK: - Lifecycle:
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupViews()
-        setupConstraints()
-    }
 }
 
 // MARK: - CustomInputStackViewDelegate:
-extension RegistationViewController: CustomInputStackViewDelegate {
+extension EnterViewController: CustomInputStackViewDelegate {
     
 }
 
 // MARK: - Setup Views:
-private extension RegistationViewController {
+private extension EnterViewController {
     func setupViews() {
         view.backgroundColor = Asset.Colors.backgroundWhite.color
-        navigationItem.title = L10n.Registration.registration
-
-        [inputsStackView, registrationButton].forEach(view.addSubview)
-        [emailStackView, passwordStackView, repeatPasswordStackView].forEach(inputsStackView.addArrangedSubview)
+        navigationItem.title = L10n.Enter.title
+        
+        [inputsStackView, enterButton].forEach(view.addSubview)
+        [emailStackView, passwordStackView].forEach(inputsStackView.addArrangedSubview)
     }
 }
 
 // MARK: - Setup Constraints:
-private extension RegistationViewController {
+private extension EnterViewController {
     func setupConstraints() {
         setupInputsStackViewConstraints()
-        setupRegistrationButton()
+        setupEnterButtonConstraints()
     }
     
     func setupInputsStackViewConstraints() {
         inputsStackView.snp.makeConstraints { make in
             make.center.equalToSuperview()
-            make.left.equalTo(GlobalUIConstants.baseInset)
-            make.right.equalTo(-GlobalUIConstants.baseInset)
         }
     }
     
-    func setupRegistrationButton() {
-        registrationButton.snp.makeConstraints { make in
-            make.top.equalTo(inputsStackView.snp.bottom).inset(-LocalUIConstants.buttonTopInset)
+    func setupEnterButtonConstraints() {
+        enterButton.snp.makeConstraints { make in
+            make.top.equalTo(inputsStackView.snp.bottom).inset(LocalUIConstants.buttonTopInset)
             make.left.equalTo(GlobalUIConstants.baseInset)
             make.right.equalTo(-GlobalUIConstants.baseInset)
         }
